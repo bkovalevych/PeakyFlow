a7cc0180e77be4039ba595e31861b53c9c9962cf
Bohdan Kovalevych <bohdan.kovalevych.official@gmail.com>
2024-03-25 00:24:49 +0200

    added tests

---
+++ src/PeakyFlow.Abstractions/Models.cs
@@ -0,0 +1,106 @@
+﻿namespace PeakyFlow.Abstractions
+{
+    public record PlayerBase(string Id, string Name);
+    
+    public record PlayerInLobby(string Id, string Name, 
+        string LobbyId, bool IsReady) 
+        : PlayerBase(Id, Name);
+
+    public record LobbyInfo(string Id, string Name, 
+        int NumberOfPlayers,  int NumberOfPlaces, 
+        string? Password, bool IsStarted = false)
+    {
+        public bool IsPublic => Password == null;
+        public bool IsFree => NumberOfPlaces > NumberOfPlayers;
+    }
+
+    public record LobbyWithDetails(LobbyInfo Lobby, IEnumerable<PlayerInLobby> Players);
+
+    public record PlayerInRoom(string Id, string Name, bool IsOnline, 
+        bool IsTakingTurn, int PassiveIncomePercent) 
+        : PlayerBase(Id, Name);
+
+    public record CurrenPlayerInRoom(PlayerInRoom PlayerInfo, PlayerRole Role, PlayerState State);
+
+    public record PlayerState(string PalyerId, int Savings, int ShouldSkeepTurns, int CanRaiseTwoDices, int Children, int Place);
+
+    public record PlayerRole(string Id, string Name, string? ImageId, string Description, 
+        PlayerRoleDetails Details, 
+        IEnumerable<AssetEntry> Assets, 
+        IEnumerable<LiabilityEntry> Liabilities,
+        IEnumerable<IncomeFlowEntry> Incomes,
+        IEnumerable<ExpenseFlowEntry> Expenses)
+    {
+        private IEnumerable<FinancialFlowEntry> Flows => new List<FinancialFlowEntry>().Union(Incomes).Union(Expenses);
+
+        public int CashFlow => Flows.Sum(x => x.Value);
+        
+        public bool IsBankrupt => CashFlow < 0; 
+    }
+
+    public record PlayerRoleDetails(int Salary, int InitialSavings, int ExpensesForOneChild);
+
+    public record FinancialEntryInfo(string Id, string Name, int Order, 
+        FinancialEntryInfo.FinancialType? EntryType, int? Percent)
+    {
+        public enum FinancialType 
+        {
+           Default,
+           Countable,
+           Percent
+        }
+    }
+
+
+
+    public abstract record FinancialFlowEntry(string Name, int Amount)
+    {
+        public virtual int Value => Amount;
+    }
+
+    public record ExpenseFlowEntry(string Name, int Amount) : FinancialFlowEntry(Name, Amount)
+    {
+        public override int Value => -Amount;
+    }
+
+    public record IncomeFlowEntry(string Name, int Amount) : FinancialFlowEntry(Name, Amount);
+
+
+    public abstract record FinancialEntry(string FinancialEntryInfoId, string Name, int Amount)
+    {
+        public virtual int Value => Amount;
+
+        public FinancialFlowEntry? GetFinancialFlowEntryByInfo(FinancialEntryInfo info, PlayerRoleDetails details, PlayerState? playerState)
+        {
+            return info.EntryType switch
+            {
+                FinancialEntryInfo.FinancialType.Countable => 
+                    InitEntry(Name, -details.ExpensesForOneChild * (playerState?.Children ?? 0)),
+                FinancialEntryInfo.FinancialType.Percent => 
+                    InitEntry(Name, (int)((info.Percent ?? 0) / 100.0  * Value)),
+                _ => null
+            };
+        }
+
+        private static FinancialFlowEntry InitEntry(string name, int value)
+        {
+            if (value < 0)
+            {
+                return new ExpenseFlowEntry(name, -value);
+            }
+
+            return new IncomeFlowEntry(name, value);
+        }
+    }
+
+    public record LiabilityEntry(string FinancialEntryInfoId, string Name, int Amount) 
+        : FinancialEntry(FinancialEntryInfoId, Name, Amount)
+    {
+        public override int Value => -Amount;
+    }
+
+    public record AssetEntry(string FinancialEntryInfoId, string Name, int Amount) 
+        : FinancialEntry(FinancialEntryInfoId, Name, Amount);
+
+    public record RoomInfo(string Id, string Name, string? Password);
+}
---
+++ tests/PeakyFlow.Abstractions.UnitTests/ModelsTests.cs
@@ -0,0 +1,71 @@
+using System.Text.Json;
+
+namespace PeakyFlow.Abstractions.UnitTests
+{
+    public class ModelsTests
+    {
+        [Fact]
+        public void TestFiannaces()
+        {
+            //Assign
+
+            var financeInfos = new List<FinancialEntryInfo>()
+            {
+                new FinancialEntryInfo("1", "Children", 1, FinancialEntryInfo.FinancialType.Countable, null),
+                new FinancialEntryInfo("3", "Home Mortgage", 3, FinancialEntryInfo.FinancialType.Percent, 1),
+                new FinancialEntryInfo("4", "Education", 4, FinancialEntryInfo.FinancialType.Percent, 8),
+                new FinancialEntryInfo("5", "Car payment", 5, FinancialEntryInfo.FinancialType.Percent, 2),
+                new FinancialEntryInfo("6", "Credit card payment", 6, FinancialEntryInfo.FinancialType.Percent, 3),
+                new FinancialEntryInfo("7", "Retail payment", 7, FinancialEntryInfo.FinancialType.Percent, 5),
+
+            };
+
+            var assetsAndLiabilities = new List<FinancialEntry>
+            {
+                new AssetEntry("0", "Savings", 40),
+                new LiabilityEntry("1", "Children", 48),
+                new LiabilityEntry("3", "Home Mortgage", 14300),
+                new LiabilityEntry("4", "Education", 0),
+                new LiabilityEntry("5", "Car payment", 1500),
+                new LiabilityEntry("6", "Credit car payment", 2200),
+                new LiabilityEntry("7", "Retail payment", 100)
+
+            };
+
+            var roleDetails = new PlayerRoleDetails(
+                assetsAndLiabilities.First(x => x.FinancialEntryInfoId == "0").Amount,
+                100,
+                assetsAndLiabilities.First(x => x.FinancialEntryInfoId == "1").Amount);
+
+            var playerState = new PlayerState("1", 100, 3, 2, 0, 3);
+
+            var financialFlows = new List<FinancialFlowEntry>()
+            {
+                new IncomeFlowEntry("Salary", 950),
+                new ExpenseFlowEntry("Taxes", 235),
+                new ExpenseFlowEntry("Other expenses", 221)
+            };
+
+            var countableFlows = financeInfos
+                .Join(
+                    assetsAndLiabilities,
+                    outer => outer.Id,
+                    inner => inner.FinancialEntryInfoId,
+                    (info, finance) => (info, finance))
+                .Select(x => x.finance.GetFinancialFlowEntryByInfo(x.info, roleDetails, playerState))
+                .Where(x => x != null && x.Amount != 0)
+                .Cast<FinancialFlowEntry>();
+
+            financialFlows.AddRange(countableFlows);
+
+            var playerRole = new PlayerRole("1", "Lawer", null, "Lawer in law",
+                roleDetails,
+                assetsAndLiabilities.Where(x => x is AssetEntry).Cast<AssetEntry>(),
+                assetsAndLiabilities.Where(x => x is LiabilityEntry).Cast<LiabilityEntry>(),
+                financialFlows.Where(x => x is IncomeFlowEntry).Cast<IncomeFlowEntry>(),
+                financialFlows.Where(x => x is ExpenseFlowEntry).Cast<ExpenseFlowEntry>());
+
+            var json = JsonSerializer.Serialize(playerRole);
+        }
+    }
+}
