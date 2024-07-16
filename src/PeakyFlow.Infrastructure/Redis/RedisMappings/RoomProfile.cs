8c19bbb7ff1e03bc8efc96738fd7b78a85e0254b
Bohdan Kovalevych <bohdan.kovalevych.official@gmail.com>
2024-07-16 12:12:39 +0300

    added updating tests

---
+++ src/PeakyFlow.Infrastructure/Redis/RedisMappings/RoomProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PeakyFlow.Abstractions.RoomAggregate;
+using PeakyFlow.Infrastructure.Redis.Models;
+
+namespace PeakyFlow.Infrastructure.Redis.RedisMappings
+{
+    public class RoomProfile : Profile
+    {
+        public RoomProfile()
+        {
+            CreateMap<RoomM, Room>()
+                .ReverseMap();
+            CreateMap<PlayerInRoomM, PlayerInRoom>()
+                .ReverseMap();
+        }
+    }
+}
--- tests/PeakyFlow.Infrastructure.IntegrationTests/Redis/RedisRepositoryTests.cs
+++ tests/PeakyFlow.Infrastructure.IntegrationTests/Redis/RedisRepositoryTests.cs
@@ -134,6 +134,77 @@ namespace PeakyFlow.Infrastructure.IntegrationTests.Redis
             Assert.NotNull(result);
         }
 
+        [Fact]
+        public async Task GivenRoom_WhenUpdate_ThenResult()
+        {
+            //Assign
+            var redisConnectionForIndeces = new RedisConnectionProvider(new ConfigurationOptions()
+            {
+                DefaultDatabase = 0,
+                EndPoints = { "localhost:6379" },
+                Ssl = false,
+                ConnectTimeout = 5000,
+                AbortOnConnectFail = false
+            });
+
+            var hosted = new RedisHostedService(redisConnectionForIndeces);
+            await hosted.StartAsync(default);
+
+            var redisConnection = new RedisConnectionProvider(new ConfigurationOptions()
+            {
+                DefaultDatabase = 0,
+                EndPoints = { "localhost:6379" },
+                Ssl = false,
+                ConnectTimeout = 5000,
+                AbortOnConnectFail = false
+            });
+
+            var rep = new RedisRepository<RoomM>(redisConnection, _fakeLogger.Object);
+
+            var result = await rep.AddAsync(new RoomM()
+            {
+                Id = "b5bcf98a-db17-44da-b2d9-ad15e07cdb35",
+                Name = "Room1",
+                Players = [
+                    new PlayerInRoomM()
+                    {
+                        Id = "46402f13-98ba-4596-b5ba-cdebaa83fbc0",
+                        Name = "BohdanChyk",
+                        Status = Abstractions.RoomAggregate.PlayerInRoomStatus.Active
+                    },
+                    new PlayerInRoomM()
+                    {
+                        Id = "46402f13-98ba-4596-b5ba-cdebaa83fbc1",
+                        Name = "Dan",
+                        Status = Abstractions.RoomAggregate.PlayerInRoomStatus.Lost
+                    }
+                ]
+            });
+
+
+            var rooms = await rep.ListAsync(new RoomMFindById("46402f13-98ba-4596-b5ba-cdebaa83fbc1"), default);
+
+            rooms[0].Name = "Changed Name";
+            rooms[0].Players[0].Status = Abstractions.RoomAggregate.PlayerInRoomStatus.Unknown;
+            rooms[0].Players = rooms[0].Players.Append(new PlayerInRoomM()
+            {
+                Id = "46402f13-98ba-4596-b5ba-cdebaa83fbc2",
+                Name = "Liuda",
+                Status = Abstractions.RoomAggregate.PlayerInRoomStatus.Out
+            })
+            .ToArray();
+
+            await rep.UpdateAsync(rooms[0]);
+            await rep.SaveChangesAsync();
+
+            await rep.DeleteAsync(result);
+            await hosted.StopAsync(default);
+
+            // assert
+
+            Assert.NotNull(result);
+        }
+
         private class RoomMFindById : Specification<RoomM>
         {
             public RoomMFindById(string id)
