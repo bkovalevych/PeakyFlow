25633755de3d63681c9a59fe748916abb7827a27
Bohdan Kovalevych <bohdan.kovalevych.official@gmail.com>
2024-08-19 23:07:51 +0300

    added imports

---
+++ src/PeakyFlow.Abstractions/GameMapAggregate/Events/PlayerStartedTurnEvent.cs
@@ -0,0 +1,6 @@
+﻿using PeakyFlow.Abstractions.RoomStateAggregate.Interfaces;
+
+namespace PeakyFlow.Abstractions.GameMapAggregate.Events
+{
+    public record PlayerStartedTurnEvent(string RoomId, string PlayerId) : IRoomStateContextEvent;
+}
