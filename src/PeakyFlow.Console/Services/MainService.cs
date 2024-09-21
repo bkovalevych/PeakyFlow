9b7f40cb56eed11434aefe7e31a2232fa31d5e07
Bohdan Kovalevych <bohdan.kovalevych.official@gmail.com>
2024-09-21 12:56:31 +0300

    temp solution

--- src/PeakyFlow.Console/Services/MainService.cs
+++ src/PeakyFlow.Console/Services/MainService.cs
@@ -1,6 +1,5 @@
-﻿using PeakyFlow.Abstractions;
-using PeakyFlow.Abstractions.GameMapAggregate;
-using PeakyFlow.Application.GameMapRules.GetMapRulesForRoom;
+﻿using PeakyFlow.GrpcProtocol.Game;
+using PeakyFlow.GrpcProtocol.Lobby;
 using Spectre.Console;
 using Spectre.Console.Rendering;
 
@@ -8,41 +7,24 @@ namespace PeakyFlow.Console.Services
 {
     internal class MainService
     {
-        private readonly IGetMapRulesForRoomService _mapGetter;
-        private Layout _layout;
-        private readonly GameMap _gameMap;
+        private readonly LobbyRpcService.LobbyRpcServiceClient _lobbyRpcService;
+        private readonly GameRpcService.GameRpcServiceClient _gameRpcService;
+        
+        private readonly Layout _layout;
         private Dictionary<int, (string, string)> _stepCords = new Dictionary<int, (string, string)>();
 
-        public MainService(IGetMapRulesForRoomService mapGetter)
+        public MainService(
+            GameRpcService.GameRpcServiceClient gameRpcService,
+            LobbyRpcService.LobbyRpcServiceClient lobbyRpcService)
         {
-            _mapGetter = mapGetter;
+            _gameRpcService = gameRpcService;
+            _lobbyRpcService = lobbyRpcService;
             _layout = new Layout("Root");
-            _gameMap = new GameMap()
-            {
-                Id = "1",
-                GameMapPlayers =
-                [
-                    new GameMapPlayer()
-                    {
-                        Id = "1", Name = "Bohdan"
-                    },
-                    new GameMapPlayer()
-                    {
-                        Id = "2", Name = "Liuda"
-                    },
-                    new GameMapPlayer()
-                    {
-                        Id = "3", Name = "Fedir"
-                    }
-                ],
-                Steps = _mapGetter.Get(default).Result!.Steps
-            };
         }
-
-
 
         public void StartMap()
         {
+            var lobbies = _lobbyRpcService.ListLobbies(new ListLobbyMessage());
             InitMap();
 
             AnsiConsole.Live(_layout)
@@ -56,29 +38,7 @@ namespace PeakyFlow.Console.Services
 
                     while (key.KeyChar != 'c' && (key.Modifiers & ConsoleModifiers.Control) == 0)
                     {
-                        for (int i = 0; i < _gameMap.GameMapPlayers.Length; i++)
-                        {
-                            //await _gameMap.MovePlayer(async eventObj =>
-                            //{
-                            //    ClearPlayers();
-
-                            //    if (eventObj.PlayerId == "1" && '1' <= key.KeyChar && '9' >= key.KeyChar)
-                            //    {
-                            //        return key.KeyChar - '0';
-                            //    }
-                            //    else if (eventObj.PlayerId != "1")
-                            //    {
-                            //        await Task.Delay(1000);
-                            //        return random.Next(1, 7);
-                            //    }
-
-                            //    return 1;
-                            //},
-                            //_ => Task.CompletedTask);
-
-                            //UpdatePlayers();
-                            //ctx.Refresh();
-                        }
+                        
 
                         key = System.Console.ReadKey();
                     }
@@ -100,28 +60,17 @@ namespace PeakyFlow.Console.Services
 
         private void ClearPlayers()
         {
-            foreach (var g in _gameMap.GameMapPlayers.GroupBy(x => x.Position))
-            {
-                var cord = _stepCords[g.Key];
-                UpdateStep(cord);
-            }
+            
         }
 
         private void UpdatePlayers()
         {
-
-            foreach (var g in _gameMap.GameMapPlayers.GroupBy(x => x.Position))
-            {
-                var cord = _stepCords[g.Key];
-
-                UpdateStep(cord, new Rows(g.Select(x => new Markup($"{x.Name}"))));
-            }
         }
 
         private void InitMap()
         {
             var columns = 5;
-            var rows = (_gameMap!.Steps.Length - 2 * columns) / 2 + 2 + 1; // +1 for start
+            var rows = (20 - 2 * columns) / 2 + 2 + 1; // +1 for start
 
             _layout.SplitColumns(Enumerable.Range(0, columns)
                 .Select(x => new Layout($"col-{x}")
@@ -134,45 +83,45 @@ namespace PeakyFlow.Console.Services
             var p = (x: 0, y: 0);
             var i = 0;
 
-            foreach (var step in _gameMap!.Steps)
-            {
-                var index = $"{p.x}-{p.y}";
-                var wrapper = step switch
-                {
-                    StepType.Salary => $"[yellow]{step} {i}[/]",
-                    StepType.Market => $"[blue]{step} {i}[/]",
-                    StepType.Children => $"[grey]{step} {i}[/]",
-                    StepType.MoneyToTheWind => $"[red]{step} {i}[/]",
-                    StepType.Deal => $"[green]{step} {i}[/]",
-                    StepType.Downsize => $"[gray]{step} {i}[/]",
-                    StepType.Charity => $"[#23ee11]{step} {i}[/]",
-                    _ => $"{step}",
-                };
+            //foreach (var step in _gameMap!.Steps)
+            //{
+            //    var index = $"{p.x}-{p.y}";
+            //    var wrapper = step switch
+            //    {
+            //        StepType.Salary => $"[yellow]{step} {i}[/]",
+            //        StepType.Market => $"[blue]{step} {i}[/]",
+            //        StepType.Children => $"[grey]{step} {i}[/]",
+            //        StepType.MoneyToTheWind => $"[red]{step} {i}[/]",
+            //        StepType.Deal => $"[green]{step} {i}[/]",
+            //        StepType.Downsize => $"[gray]{step} {i}[/]",
+            //        StepType.Charity => $"[#23ee11]{step} {i}[/]",
+            //        _ => $"{step}",
+            //    };
 
-                var text = $"{wrapper}";
-                var cord = (index, text);
-                _stepCords[i] = cord;
+            //    var text = $"{wrapper}";
+            //    var cord = (index, text);
+            //    _stepCords[i] = cord;
 
-                UpdateStep(cord);
+            //    UpdateStep(cord);
 
-                ++i;
-                var next = (x: p.x + vec.x, y: p.y + vec.y);
+            //    ++i;
+            //    var next = (x: p.x + vec.x, y: p.y + vec.y);
 
-                if (next.x < 0 || next.y < 0 || next.x >= columns || next.y >= rows)
-                {
-                    vec = RotateVec(vec);
-                    p = (x: p.x + vec.x, y: p.y + vec.y);
+            //    if (next.x < 0 || next.y < 0 || next.x >= columns || next.y >= rows)
+            //    {
+            //        vec = RotateVec(vec);
+            //        p = (x: p.x + vec.x, y: p.y + vec.y);
 
-                    continue;
-                }
+            //        continue;
+            //    }
 
-                p = next;
+            //    p = next;
 
-                if (i == 1)
-                {
-                    p = (x: 0, y: 1);
-                }
-            }
+            //    if (i == 1)
+            //    {
+            //        p = (x: 0, y: 1);
+            //    }
+            //}
         }
 
         private void UpdateStep((string index, string text) cord, IRenderable? renderable = null)
