d7fb614c00bc119fb8ae01dfa92deebe0e0e1b3f
Bohdan Kovalevych <bohdan.kovalevych.official@gmail.com>
2024-09-21 12:56:14 +0300

    add config for console

--- src/PeakyFlow.Console/Program.cs
+++ src/PeakyFlow.Console/Program.cs
@@ -1,23 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using PeakyFlow.Application.Common.Extensions;
-using PeakyFlow.Application.GameMapRules.GetMapRulesForRoom;
 using PeakyFlow.Console.Services;
+using PeakyFlow.GrpcProtocol.Game;
+using PeakyFlow.GrpcProtocol.Lobby;
 
 var host = Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(conf =>
+        conf.AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+    )
     .ConfigureServices((context, services) =>
     {
         services
-            //.AddLogging(x => x.AddDebug().AddConsole().SetMinimumLevel(LogLevel.Information))
-            .AddApplication()
-            .AddSingleton<MainService>()
-            .AddTransient<IGetMapRulesForRoomService, GetMapRulesService>();
+        //.AddLogging(x => x.AddDebug().AddConsole().SetMinimumLevel(LogLevel.Information))
+        .AddSingleton<MainService>()
+        .AddGrpcClient<LobbyRpcService.LobbyRpcServiceClient>(conf =>
+        {
+            conf.Address = new Uri(context.Configuration.GetConnectionString("Server")!);
+        });
+        services.AddGrpcClient<GameRpcService.GameRpcServiceClient>(conf =>
+        {
+            conf.Address = new Uri(context.Configuration.GetConnectionString("Server")!);
+        });
     })
     .Build();
 
-host.StartAsync().Wait();
+await host.StartAsync();
 var main = host.Services.GetRequiredService<MainService>();
 
 main.StartMap();
