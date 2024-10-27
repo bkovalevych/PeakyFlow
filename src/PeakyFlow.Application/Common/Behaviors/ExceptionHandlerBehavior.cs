37a2e8303bd566d6ed4e0b45597838a210b2a3be
Bohdan Kovalevych <bohdan.kovalevych.official@gmail.com>
2024-10-27 12:33:48 +0200

    added handler for precondition failed

---
+++ src/PeakyFlow.Application/Common/Behaviors/ExceptionHandlerBehavior.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+
+namespace PeakyFlow.Application.Common.Behaviors
+{
+    internal class ExceptionHandlerBehavior<TRequest, TResponse>([FromKeyedServices("default")] ResiliencePipeline resiliencePipeline) : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            return await resiliencePipeline.ExecuteAsync(async _ => await next());
+        }
+    }
+}
