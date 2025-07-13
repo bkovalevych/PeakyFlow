f4ccc8bf28106fcdcf2f13694105d4ff0499926a
bohdan.kovalevych <bohdan.kovalevych@outlook.com>
2025-07-13 21:25:43 +0300

    removed redundant files

--- src/PeakyFlow.Server/Program.cs
+++ src/PeakyFlow.Server/Program.cs
@@ -38,25 +38,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseGrpcWeb(new GrpcWebOptions() { DefaultEnabled = true });
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.MapGrpcService<LobbyGrpcService>();
 app.MapGrpcService<GameGrpcService>();
@@ -64,7 +46,4 @@ app.MapGrpcService<GameGrpcService>();
 app.MapDefaultEndpoints();
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
