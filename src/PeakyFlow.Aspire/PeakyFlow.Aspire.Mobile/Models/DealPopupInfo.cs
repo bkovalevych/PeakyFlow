5ae6710696cead8e03c319c709fab8ede77420b4
bohdan_k <bohdan_k@optimove.com>
2025-08-16 01:28:41 +0300

    feat: implement game popups and player movement mechanics with win condition

---
+++ src/PeakyFlow.Aspire/PeakyFlow.Aspire.Mobile/Models/DealPopupInfo.cs
@@ -0,0 +1,11 @@
+namespace PeakyFlow.Aspire.Mobile.Models;
+
+public class DealPopupInfo
+{
+    public string Description { get; set; } = "";
+    public string Footer { get; set; } = "";
+    public int DownPay { get; set; }
+    public int Cost { get; set; }
+    public int CashFlow { get; set; }
+    public int Debt { get; set; }
+}
